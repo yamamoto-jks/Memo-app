18b0464077ac3d8389970d46c142419c4afceda1
yamamoto-jks <yamamoto-daiki@jpn-ks.co.jp>
2026-05-08 01:24:17 +0000

    tailwind.css適用

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/js/app.js')
+        @vite(['resources/css/app.css', 'resources/js/app.ts'])
         <x-inertia::head />
     </head>
     <body>
