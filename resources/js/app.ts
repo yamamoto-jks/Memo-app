cd25e2479f150979bcc1e1ca86d0c6b2434af1b9
yamamoto-jks <yamamoto-daiki@jpn-ks.co.jp>
2026-05-08 01:08:21 +0000

    TypeScript setup

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,5 +2,5 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name: string) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
 })
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import inertia from '@inertiajs/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
